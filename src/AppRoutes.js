a304e109fa6408931e887c8ea30a0851251c2ea8
tangweishu <tangweishu_@126.com>
2016-11-24 13:48:57 +0800

    更新了页面索引文件

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,20 +1,13 @@
 /**
  * Created by tangweishu on 16/9/19.
  */
-import Home from './Scenes/S_Home';
-import LessonMenus from './Scenes/S_LessonMenus';
+import Home from './Scenes/S_Home'
+import LessonMenus from './Scenes/S_LessonMenus'
 import StrokersOrder from './Scenes/S_StrokersOrder';
 import StrokersWrite from './Scenes/S_StrokersWrite';
-import Test from './Scenes/S_Test';
-/*
-import Main from './Component/MainTabView' 
-import Show1 from './Scenes/P_Show1'
-import Show2 from './Scenes/P_Show2'
-import PushScene from './Scenes/P_PushScene'
-import PushScene2 from './Scenes/P_PushScene2'
-import Example from './Component/ExampleTabView'
-import Embed from './Component/EmbedTabView'
-import UserInfo from './Scenes/P_UserInfo'*/
+import Practice from './Scenes/S_Practice'
+import Test from './Scenes/S_Test'
+
 let RouteList = [
     {
         name:'Home',
@@ -30,9 +23,10 @@ let RouteList = [
         configure:'FFB'
     },
     {
-        name:'Test',
-        component:Test,
+        name:'Practice',
+        component:Practice,
         index:2,
+        configure:'FFB'
     },
     {
         name:'StrokersOrder',
@@ -46,73 +40,22 @@ let RouteList = [
         index: 4,
         configure: 'FFR',
     },
-   /* {
-        name:'Main',
-        component:Main,
-        type:'TabView',
-        index:0,
-        showStatusBar:false,
-    },
-    
     {
-        name:'Show1',
-        component:Show1,
-        index:2,
-        showStatusBar:false,
-        configure:'FFL',
-    },
-    {
-        name:'Show2',
-        component:Show2,
-        index:3,
-        configure:'FFL',
-    },
-    {
-        name:'PushScene',
-        component:PushScene,
-        index:4,
-        configure:'FFB',
-        showStatusBar:false,
-    },
-    {
-        name:'PushScene2',
-        component:PushScene2,
+        name:'Test',
+        component:Test,
         index:5,
-        configure:'FFL',
+        showStatusBar:false,
     },
-    {
-        name:'Example',
-        component:Example,
-        type:'TabView',
-        index:6,
-    },
-    {
-        name:'Embed',
-        component:Embed,
-        type:'TabView',
-        index:7,
-    },
-    {
-        name:'UserInfo',
-        component:UserInfo,
-        index:8,
-    }*/
+
 ];
 
 let RouteIndex = {
     Home:0,
     LessonMenus:1,
-    Test:2,
+    Practice:2,
     StrokersOrder: 3,
     StrokersWrite: 4,
-    /*Main:1,   
-    Show1:2,
-    Show2:3,
-    PushScene:4,
-    PushScene2:5,
-    Example:6,
-    Embed:7,
-    UserInfo:8,*/
+    Test:5,
 }
  
 module.exports = {RouteList,RouteIndex};
